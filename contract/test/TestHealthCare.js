f09b9954acd026b4cb13ceb7edcc97cc01db95f8
rk-10 <khanejarohan07@gmail.com>
2018-10-12 22:44:12 +0530

    adds tests

--- contract/test/TestHealthCare.js
+++ contract/test/TestHealthCare.js
@@ -5,13 +5,23 @@
 const HealthCare = artifacts.require("HealthCareRecords");
 
 contract("HealthCare tests", function (accounts) {
-    it("First test", function () {
+    it("Owner is account 0", function () {
         return HealthCare.deployed()
             .then((Instance) => {
                 return Instance.owner();
             })
             .then((_owner) => {
                 assert.equal(_owner, accounts[0]);
+            })
+    });
+
+    it("owner changed", function () {
+        return HealthCare.deployed()
+            .then((Instance) => {
+                return Instance.changeOwner(accounts[2]);
+            })
+            .then((changed) => {
+                assert.equal(changed, true);
             })
     });
 });
